bb60cd0e3ddc62cf755f95a6c014f65a87c12f42
Jafar <ikbal.jafar46@gmail.com>
2023-11-12 20:57:03 +0530

    test commit

--- act.php
+++ act.php
@@ -6,8 +6,8 @@ $sl=$_REQUEST['sl'];
 $stat=$_REQUEST['stat'];
 echo "update main_product set stat='$stat' where sl='$sl'";
 $datatt= mysqli_query($conn,"update main_product set stat='$stat' where sl='$sl'")or die(mysqli_error($conn));
-
+///test
 ?>
 <script>
-get_list();
+get_list(); 
 </script>
